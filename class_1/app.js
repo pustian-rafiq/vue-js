4da046e0ce94ca1064b3f823f5536b00ced30290
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2020-09-16 10:06:55 +0600

    vue js practice code

---
+++ class_1/app.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+   
+    el:'#app',
+    data: {
+        name: 'Namkora.com'
+    }
+    /*methods:{
+
+        Amount(taka){
+            this.dollar +=taka
+        }
+    }*/
+
+})
